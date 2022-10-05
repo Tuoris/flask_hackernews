6068a9324d5d6d81c450b4f979191c912661926f
Tuoris <tuoris.dev@gmail.com>
2022-10-05 09:20:29 +0300

    Use official API

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-NEWS_API_URL = "https://node-hnapi.herokuapp.com/news"
-ITEMS_API_URL = "https://node-hnapi.herokuapp.com/item"
+NEWS_API_URL = "https://api.hackerwebapp.com/news"
+ITEMS_API_URL = "https://api.hackerwebapp.com/item"
 
 NEWS_SAMPLE_FILENAME = "sample_news_page.json"
 ITEM_SAMPLE_FILENAME = "sample_item_page.json"
